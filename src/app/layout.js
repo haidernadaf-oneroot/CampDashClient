280d684eda99cbcab16eab08e9d76d4dda05bf69
HaiderNadaf <haidernadaf67@gmail.com>
2025-04-17 19:33:38 +0530

    Refactor layout component: simplify structure by removing redundant Sidebar and wrapping body content directly

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,18 +29,8 @@ export default function RootLayout({ children }) {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title>Markhetting</title>
         </head>
-        <Sidebar className="fixed left-0 top-0 w-52 h-screen bg-green-800 shadow-lg z-50" />
-        <body className="antialiased bg-gray-100">
-        <div className="flex min-h-screen bg-gray-100">
-          <Sidebar className="fixed left-0 top-0 w-52 h-screen bg-green-800 shadow-lg z-50" />
-          <div className="flex-1 flex flex-col">
-            <Topbar className="fixed top-0 left-52 right-0 h-16 bg-white shadow-md z-40" />
-            <main className="flex-1 transition-all duration-300 ease-in-out ml-52 mt-16 p-4">
-              {children}
-            </main>
-          </div>
-        </div>
-      </body>
+        
+        <body className="antialiased bg-gray-100">{children}</body>
       </html>
     );
   }
