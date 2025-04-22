122190219a64c6f78845538cb280f245ebce0b12
HaiderNadaf <haidernadaf67@gmail.com>
2025-04-22 14:50:15 +0530

    Refactor Update component: implement loading state and handle database update

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { Loader, RotateCw } from "lucide-react";
 
 const Update = () => {
-  const handleUpadte = () => {
-    console.log("update");
+  const [loading, setLoading] = useState(false);
+
+  const handleUpdate = async () => {
+    setLoading(true);
+    const url = "http://localhost:3005/update-database";
+
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+      alert(JSON.stringify(data));
+    } catch (error) {
+      console.error("Error updating:", error);
+      alert("Failed to update database.");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <div className="flex justify-end">
-      <button
-        className="w-20 h-7 border rounded-lg bg-green-600"
-        onClick={handleUpadte}
+      <div
+        onClick={!loading ? handleUpdate : undefined}
+        className={`w-14 h-10 rounded-full text-black flex items-center justify-center cursor-pointer ${
+          loading ? "opacity-70 pointer-events-none" : ""
+        }`}
       >
-        update
-      </button>
+        {loading ? (
+          <Loader className="h-10 w-10 animate-spin text-green-600" />
+        ) : (
+          <Loader className="h-5 w-5" />
+        )}
+      </div>
     </div>
   );
 };
