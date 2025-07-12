8f0b462a82a4e0c457afcb6c674d3af7c449ee94
HaiderNadaf <haidernadaf67@gmail.com>
2025-07-12 15:04:54 +0530

    feat: update login page layout with improved styling and responsiveness

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -72,37 +72,41 @@ export default function Login() {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-20 bg-white p-6 rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold mb-6 text-center text-black">Login</h2>
-      {error && <p className="text-red-600 mb-4 text-sm">{error}</p>}
+    <div className="w-full h-screen flex items-center justify-center bg-purple-600">
+      <div className="max-w-md mx-auto border-yellow-700 border-2 border-y-yellow-700  p-6 rounded-lg  bg-purple-200 shadow-2xl">
+        <h2 className="text-2xl font-bold mb-6 text-center text-black">
+          Login
+        </h2>
+        {error && <p className="text-red-600 mb-4 text-sm">{error}</p>}
 
-      <form onSubmit={handleSubmit} className="space-y-4 text-black">
-        <input
-          type="tel"
-          name="phoneNumber"
-          placeholder="Phone Number"
-          value={formData.phoneNumber}
-          onChange={handleChange}
-          className="w-full p-2 border rounded"
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-          className="w-full p-2 border rounded"
-          required
-        />
-        <button
-          type="submit"
-          className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
-          disabled={loading}
-        >
-          {loading ? "Logging in..." : "Login"}
-        </button>
-      </form>
+        <form onSubmit={handleSubmit} className="space-y-4 text-black">
+          <input
+            type="tel"
+            name="phoneNumber"
+            placeholder="Phone Number"
+            value={formData.phoneNumber}
+            onChange={handleChange}
+            className="w-full p-2 border rounded"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+            className="w-full p-2 border rounded"
+            required
+          />
+          <button
+            type="submit"
+            className="w-full bg-purple-600 text-white py-2 rounded hover:bg-purple-700"
+            disabled={loading}
+          >
+            {loading ? "Logging in..." : "Login"}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
